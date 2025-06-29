67eb24afc7bce7e510f043982d4e4decddde7580
진호정 (Hojung Jin) <jhojung0311@gmail.com>
2025-06-29 15:18:39 +0900

    250629_151837

---
+++ 02 Deep Dive into C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using GiftBoxManagerNamespace;
+using GiftBoxNamespace;
+
+namespace ProgramNamespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GiftBoxManager giftBoxManager = new GiftBoxManager();
+            giftBoxManager.SetInit();
+        }
+    }
+}
